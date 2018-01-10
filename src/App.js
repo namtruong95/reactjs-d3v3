81632be38f0c25bf3e81625d8e48ca27b4b281eb
nam.tt <nam.tt@neo-lab.vn>
2018-01-10 14:19:41 +0700

    initilize

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import D3v3 from './components/d3v3'
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <D3v3 />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
